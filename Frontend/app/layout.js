7ce5a2807386bf37e658448cd8e2ad0b9cc5b0fa
aasishhumagain <aasishhumagain1111@gmail.com>
2026-04-25 10:58:15 +0545

    Fixed bugs

--- Frontend/app/layout.js
+++ Frontend/app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className="h-full antialiased" data-scroll-behavior="smooth">
       <body className="min-h-full bg-[--background] text-[--foreground]">{children}</body>
     </html>
   );
